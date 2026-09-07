3f6dc7a26cbc0ec111194fcf809ed6af3a54cc78
Aditya Rahi <95748245+adityarahi@users.noreply.github.com>
2026-09-08 02:27:31 +0530

    Add: buy-and-sell-crypto-with-cooldown - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<int> sold(n+1), held(n+1), reset(n+1);
+        reset[0] = 0;
+        sold[0] = INT_MIN;
+        held[0] = INT_MIN;
+        for(int i = 1; i <= n; i++) {
+            sold[i] = prices[i-1] + held[i-1];
+            held[i] = max(held[i-1], reset[i-1] - prices[i-1]);
+            reset[i] = max(reset[i-1], sold[i-1]);
+        }
+        return max(sold[n], reset[n]);
+    }
+};
